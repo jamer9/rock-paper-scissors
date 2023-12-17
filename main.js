91460e2ec20a6a37cad24d169af6fbc8765c8743
jacob <124761627+jamer9@users.noreply.github.com>
2023-12-17 13:55:35 -0500

    Draft game function

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
+//global variables
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+let computerChoice;
+
+
+
 //function that randomly generates rock, paper, or scissors
 function getComputerChoice () {
     let number = Math.floor(Math.random() * 10 + 1) * 3;
@@ -21,33 +29,59 @@ function playRound (playerChoice, computerChoice) {
     }
     else if (playerChoice == 'rock' && computerChoice == 'paper') {
         return 'You win! Rock beats paper.';
+        playerScore += 1;
     }  
     else if (playerChoice == 'paper' && computerChoice == 'rock') {
         return 'You lose! Rock beats paper.';
+        computerScore += 1;
     }
     else if (playerChoice == 'scissors' && computerChoice == 'paper') {
         return 'You win! Scissors beats paper.';
+        playerScore += 1;
     }
     else if (playerChoice == 'paper' && computerChoice == 'scissors') {
         return 'You lose! Scissors beats paper.';
+        computerScore += 1;
     }
     else if (playerChoice == 'scissors' && computerChoice == 'rock') {
         return 'You lose! Rock beats scissors.';
+        computerScore += 1;
     }
     else if (playerChoice == 'rock' && computerChoice == 'scissors') {
         return 'You win! Rock beats scissors.';
+        playerScore += 1;
     }
 }
-
+/*
 const playerSelection = "rock";
 console.log(playerSelection);
 const computerSelection = getComputerChoice();
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
-
+*/
 
 //if tie replay game
 
+
+//game
+function game () {
+    while (playerScore < 5 && computerScore < 5) {
+        playerChoice = prompt('Enter you choice:').toLowerCase();
+        computerChoice = getComputerChoice();
+        //console.log(playerChoice);
+        console.log(computerChoice);
+        playRound(playerChoice, computerChoice);
+    }
+    if (playerScore == 5) {
+        return 'You won the game!';
+    }
+    else {
+        return 'Sorry, the computer won the game';
+    }
+}
+
+game();
 //get user input
+
 
 //call functions and log the round
