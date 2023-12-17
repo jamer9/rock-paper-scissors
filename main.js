f77f85d36387a88be9b768d136ee42dde3e269f2
jacob <124761627+jamer9@users.noreply.github.com>
2023-12-17 12:39:18 -0500

    Add 1st draft of playRound function and test

--- main.js
+++ main.js
@@ -2,22 +2,50 @@
 //function that randomly generates rock, paper, or scissors
 function getComputerChoice () {
     let number = Math.floor(Math.random() * 10 + 1) * 3;
-    console.log(number);
     let choice;
     if (number <= 10) {
-        choice = 'Rock';
+        choice = 'rock';
     }
     else if (number <= 20) {
-        choice = 'Paper';
+        choice = 'paper';
     }
     else {
-        choice = 'Scissors';
+        choice = 'scissors';
     }
     return choice;
 }
-console.log(getComputerChoice());
 
-//function that compares and decides winner
+function playRound (playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        return 'Tie, choose again';
+    }
+    else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        return 'You win! Rock beats paper.';
+    }  
+    else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        return 'You lose! Rock beats paper.';
+    }
+    else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        return 'You win! Scissors beats paper.';
+    }
+    else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        return 'You lose! Scissors beats paper.';
+    }
+    else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        return 'You lose! Rock beats scissors.';
+    }
+    else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        return 'You win! Rock beats scissors.';
+    }
+}
+
+const playerSelection = "rock";
+console.log(playerSelection);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+
 //if tie replay game
 
 //get user input
