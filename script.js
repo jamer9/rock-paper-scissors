9a84ab65871a943bc16b8d9ffe20bf26f87b19bd
jacob <124761627+jamer9@users.noreply.github.com>
2023-12-17 12:07:33 -0500

    Add first draft of getComputerChoice function

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//function that randomly generates rock, paper, or scissors
+function getComputerChoice () {
+    let number = floor(Math.random() * 10 + 1) * 3;
+    console.log(number);
+    let choice;
+    if (number <= 10) {
+        choice = 'Rock';
+    }
+    else if (number <= 20) {
+        choice = 'Paper';
+    }
+    else {
+        choice = 'Scissors';
+    }
+    return choice;
+}
+console.log(getComputerChoice());
+
+//function that compares and decides winner
+//if tie replay game
+
+//get user input
+
+//call functions and log the round
